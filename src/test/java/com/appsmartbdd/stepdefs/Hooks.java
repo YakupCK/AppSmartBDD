1110e390983a37ec9a63ac6fe371428bd239c6b9
YakupCK <yakupckahveci@gmail.com>
2022-04-22 15:14:34 +0300

    first commit

---
+++ src/test/java/com/appsmartbdd/stepdefs/Hooks.java
@@ -0,0 +1,43 @@
+package com.appsmartbdd.stepdefs;
+
+import com.appsmartbdd.utils.Driver;
+import com.appsmartbdd.utils.PropertyReader;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    public static WebDriver driver;
+
+    @Before("not @ChromeProfile")
+    public void setUp(){
+        driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Long.parseLong(PropertyReader.getProperty("implicitWait")), TimeUnit.SECONDS);
+    }
+
+    @Before("@ChromeProfile")
+    public void setUpForChromeProfile(){
+        driver = Driver.getDriverChromeProfile();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Long.parseLong(PropertyReader.getProperty("implicitWait")), TimeUnit.SECONDS);
+    }
+
+    @After()
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.quitDriver();
+    }
+
+
+
+}
