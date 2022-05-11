7a19f48421fb79ca4679fa2695a996a7a99f6415
YakupCK <97432469+YakupCK@users.noreply.github.com>
2022-05-11 10:30:09 +0300

    Update FailedTestRunner.java
    failedTestRunner

--- src/test/java/com/appsmartbdd/runners/FailedTestRunner.java
+++ src/test/java/com/appsmartbdd/runners/FailedTestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 		plugin = {"html:target/failed-html-report"},
 		features = "@target/rerun.txt",
-		glue = "com/bestbuy/stepdefs"
+		glue = "com/appsmartbdd/stepdefs"
 )
 
 public class FailedTestRunner {
